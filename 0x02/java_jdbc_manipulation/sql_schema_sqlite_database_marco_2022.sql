8883a0574c875d7fef33df61218e4f6825d3ab8f
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-06-15 11:47:41 -0300

    task 2. Abrir o banco de dados no SQLite Online

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE cliente (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome varchar(255) NOT NULL, idade INTEGER NOT NULL, cpf varchar(14) NOT NULL, rg varchar(14) NOT NULL);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
